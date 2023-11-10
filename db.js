f9ddfcd9a4fde61bd602a0b43ca788a8a102a369
Arthur Honorio <hno.arthur@gmail.com>
2023-11-10 13:36:36 -0300

    postgres

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
+
 module.exports = pool;
